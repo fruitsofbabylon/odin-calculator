0796010c7de3fb86f94f748421511fbc65940f09
Anna Orlova <orlovanna412@gmail.com>
2025-01-30 19:39:54 +0000

    formatting + fix for the multiple operands bug

--- javascript.js
+++ javascript.js
@@ -1,54 +1,54 @@
 // add 2 numbers and an operand variables
-let firstOperand = '';
-let secondOperand = '';
-let operation = '';
+let firstOperand = "";
+let secondOperand = "";
+let operation = "";
 
 //basic math operators
-function add (a, b) {
+function add(a, b) {
     return a + b;
-};
+}
 
-function sub (a, b) {
+function sub(a, b) {
     return a - b;
-};
+}
 
-function mul (a, b) {
+function mul(a, b) {
     return a * b;
-};
+}
 
-function div (a, b) {
+function div(a, b) {
     return a / b;
-};
+}
 
 //result of math operators combined with operands
-function operate (operator, a, b) {
-    switch(operator) {
-        case '+':
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
             return add(a, b);
 
-        case '-':
+        case "-":
             return sub(a, b);
 
-        case '*':
+        case "*":
             return mul(a, b);
-        
-        case '/':
+
+        case "/":
             if (b == 0) {
-                return 'not today';
+                return "not today";
             }
             return div(a, b);
     }
 }
 
 //display the content of digit buttons clicked and store their values
-const display = document.querySelector('.display');
-const digitButtons = document.querySelectorAll('.operand, .decimal');
-let currentNumber = '';
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".operand, .decimal");
+let currentNumber = "";
 let shouldResetDisplay = false;
 
 function updateDisplay(digit) {
     if (shouldResetDisplay) {
-        display.textContent = '';
+        display.textContent = "";
         shouldResetDisplay = false;
     }
     if (currentNumber.length >= 14) {
@@ -58,35 +58,46 @@ function updateDisplay(digit) {
     display.textContent += digit;
 }
 
-digitButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        updateDisplay(button.textContent)
-    })
-})
+digitButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        updateDisplay(button.textContent);
+    });
+});
 
 //store the first operand and operator clicked
-const operatorButtons = document.querySelectorAll('.operator');
+const operatorButtons = document.querySelectorAll(".operator");
 
-operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (firstOperand === '') {
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (firstOperand === "") {
             firstOperand = currentNumber;
         } else {
-            firstOperand = parseFloat(operate(operation, parseFloat(firstOperand), parseFloat(secondOperand)).toPrecision(14));
+            firstOperand = parseFloat(
+                operate(
+                    operation,
+                    parseFloat(firstOperand),
+                    parseFloat(currentNumber)
+                ).toPrecision(14)
+            );
             display.textContent = firstOperand;
         }
-        currentNumber = '';
+        currentNumber = "";
         operation = button.textContent;
         shouldResetDisplay = true;
-    })
-})
+    });
+});
 
 //store the second operand and execute the calculation
-const resultButton = document.querySelector('.equals');
+const resultButton = document.querySelector(".equals");
 
-resultButton.addEventListener('click', () => {
+resultButton.addEventListener("click", () => {
     secondOperand = currentNumber;
-    currentNumber = '';
-    display.textContent = parseFloat(operate(operation, parseFloat(firstOperand), parseFloat(secondOperand)).toPrecision(14));
-})
-
+    currentNumber = "";
+    display.textContent = parseFloat(
+        operate(
+            operation,
+            parseFloat(firstOperand),
+            parseFloat(secondOperand)
+        ).toPrecision(14)
+    );
+});
